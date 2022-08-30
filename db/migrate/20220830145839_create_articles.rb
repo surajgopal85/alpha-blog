b5b091c34da5f2b244cd67538ba7e93ba8c1b1e0
Suraj Gopal <surajcancode@gmail.com>
2022-08-30 12:05:49 -0400

    added create_articles, schema

---
+++ db/migrate/20220830145839_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      # t is object, .string is data type, :title is attribute name (as symbol)
+      t.string :title
+      t.timestamps
+    end
+  end
+end
