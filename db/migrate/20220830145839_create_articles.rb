0253d7b04ab10b67333332e61dfc06dd5a6ded05
Suraj Gopal <surajcancode@gmail.com>
2022-08-30 12:17:21 -0400

    added description attribute as text data to db

--- db/migrate/20220830145839_create_articles.rb
+++ db/migrate/20220830145839_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       # t is object, .string is data type, :title is attribute name (as symbol)
       t.string :title
+      t.text :description
       t.timestamps
     end
   end
