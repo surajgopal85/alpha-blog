e8b2a81ccef66069387b75946290ce9fce0ac9e8
Suraj Gopal <surajcancode@gmail.com>
2022-08-31 13:49:04 -0400

    added create, update, edit methods, pages. NOTE - error messages for create with blank/short fields not yet working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  # add articles routes to routes
+  # only: ... enumerates which categories added
+  # step 1 of adding a new route
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
